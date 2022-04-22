3f7b03ea82da38636ab888050e39f49ac2f46376
FCM <rahmawatidesi730@gmail.com>
2022-04-22 14:13:05 +0700

    Fragment Excampel2

---
+++ app/src/main/java/com/example/fragmentexcampel/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.fragmentexcampel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), Coordinator{
+
+    lateinit var fragBookDescription: BookDescription
+    lateinit var fragBookTitle: BookTitles
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fragBookTitle = BookTitles()
+        fragBookDescription = BookDescription()
+        val fragTransaction = supportFragmentManager.beginTransaction()
+        fragTransaction.add(R.id.fragtop, fragBookTitle)
+        fragTransaction.add(R.id.fragbottom, fragBookDescription)
+        fragTransaction.commit()
+    }
+
+    override fun onBookChanged(index: Int) {
+        fragBookDescription.changeDescription(index)
+    }
+
+}
